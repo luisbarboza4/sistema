30294fc81e1026078cbe967b25c8230e53314550
luisbarboza4 <luisbarboza1990@gmail.com>
2016-11-02 18:08:56 -0400

    Add files via upload

--- models/usuario.php
+++ models/usuario.php
@@ -39,7 +39,7 @@
 
 		function lista_users($valor,$cantidad,$pag)
 		{
-			$sql = "SELECT id_usuario, nombre_usuario, apellido_usuario, email_usuario, username, level, id_colegio, id_circuito, telefono FROM usuarios WHERE nombre_usuario like '%".$valor."%' or apellido_usuario like '%".$valor."%' or email_usuario like '%".$valor."%' or username like '%".$valor."%' ORDER BY level ASC LIMIT ".$pag." OFFSET ".$cantidad."";
+			$sql = "SELECT id_usuario, nombre_usuario, apellido_usuario, email_usuario, username, level, id_colegio, id_circuito, telefono FROM usuarios WHERE level!=0 AND (nombre_usuario like '%".$valor."%' or apellido_usuario like '%".$valor."%' or email_usuario like '%".$valor."%' or username like '%".$valor."%') ORDER BY level ASC LIMIT ".$pag." OFFSET ".$cantidad."";
 			$this->conexion->conexion->set_charset('utf8');
 			$resultados = $this->conexion->conexion->query($sql);
 			$arreglo = array();
@@ -73,7 +73,7 @@
 
 		function cantidad_users($valor)
 		{
-			$sql = "SELECT id_usuario, nombre_usuario, apellido_usuario, email_usuario, username, level, id_colegio, id_circuito, telefono FROM usuarios WHERE nombre_usuario like '%".$valor."%' or apellido_usuario like '%".$valor."%' or email_usuario like '%".$valor."%' or username like '%".$valor."%' ORDER BY level ASC";
+			$sql = "SELECT id_usuario, nombre_usuario, apellido_usuario, email_usuario, username, level, id_colegio, id_circuito, telefono FROM usuarios WHERE level!=0 AND (nombre_usuario like '%".$valor."%' or apellido_usuario like '%".$valor."%' or email_usuario like '%".$valor."%' or username like '%".$valor."%') ORDER BY level ASC";
 			$this->conexion->conexion->set_charset('utf8');
 			$resultados = $this->conexion->conexion->query($sql);
 			$arreglo = array();
@@ -133,21 +133,21 @@
 		}
 
 		function nombres($level,$id_circuito,$id_colegio){
-			if($level==1){
-				$sql = "SELECT cir.nombre_circuito FROM circuito cir WHERE cir.id_circuito='$id_circuito'";
-			}
-			if($level==2){
-				$sql = "SELECT cir.nombre_circuito,co.nombre_colegio FROM circuito cir, colegio co WHERE cir.id_circuito='$id_circuito' AND co.id_colegio='$id_colegio'";
-			}
-			$this->conexion->conexion->set_charset('utf8');
-			$resultados = $this->conexion->conexion->query($sql);
-			$arreglo = array();
-			while ($re=$resultados->fetch_array(MYSQL_NUM)) {
-				$arreglo[0]=$re[0];
-				$arreglo[1]=$re[1];
-			}
-			return $arreglo;
-			$this->conexion->cerrar();
+				if($level==1){
+					$sql = "SELECT cir.nombre_circuito FROM circuito cir WHERE cir.id_circuito='$id_circuito'";
+				}
+				if($level==2){
+					$sql = "SELECT cir.nombre_circuito,co.nombre_colegio FROM circuito cir, colegio co WHERE cir.id_circuito='$id_circuito' AND co.id_colegio='$id_colegio'";
+				}
+				$this->conexion->conexion->set_charset('utf8');
+				$resultados = $this->conexion->conexion->query($sql);
+				$arreglo = array();
+				while ($re=$resultados->fetch_array(MYSQL_NUM)) {
+					$arreglo[0]=$re[0];
+					$arreglo[1]=$re[1];
+				}
+				return $arreglo;
+				$this->conexion->cerrar();
 		}
 
 		function editprofile($id,$nombres,$apellidos,$email,$telefono,$pregunta,$respuesta){
